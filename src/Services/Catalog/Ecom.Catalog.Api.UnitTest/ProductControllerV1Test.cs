97e77181f20600327ddbfa82f58d28c3ad5e19ba
Taylan Tutar <tylntutar@gmail.com>
2023-04-20 16:56:09 +0300

    Product Get Test

--- src/Services/Catalog/Ecom.Catalog.Api.UnitTest/ProductControllerV1Test.cs
+++ src/Services/Catalog/Ecom.Catalog.Api.UnitTest/ProductControllerV1Test.cs
@@ -2,19 +2,20 @@ using Ecom.Catalog.Model;
 using Ecom.Catalog.Service.Abstract;
 using Ecom.Catalog.Api.Controllers.v1;
 using Moq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ecom.Catalog.Api.UnitTest
 {
     public class ProductControllerV1Test
     {
-        private readonly Mock<IProductService> _mocRepository;
+        private readonly Mock<IProductService> _mocService;
         private readonly ProductController _productController;
         private readonly List<GetAllProductDto> _products;
 
         public ProductControllerV1Test()
         {
-            _mocRepository = new Mock<IProductService>();
-            _productController = new ProductController(_mocRepository.Object);
+            _mocService= new Mock<IProductService>();
+            _productController = new ProductController(_mocService.Object);
 
             _products = new List<GetAllProductDto>{
                 new GetAllProductDto{Id=Guid.NewGuid(),Name="Defter",Category= "A"},
@@ -28,9 +29,16 @@ namespace Ecom.Catalog.Api.UnitTest
         [Fact]
         public async void GetProducts_ExecuteAction_ReturnOkWithProducts()
         {
-            _mocRepository.Setup(_ => _.GetAll()).Returns(_products);
+            _mocService.Setup(_ => _.GetAll()).Returns(_products);
 
-            //var result = await _productController.Get();
+            var result = await _productController.Get();
+
+            var ok = Assert.IsType<OkObjectResult>(result);
+
+            var products = Assert.IsAssignableFrom<List<GetAllProductDto>>(ok.Value);
+
+            Assert.Equal(products.Count ,_products.Count);
+
         }
     }
 }
